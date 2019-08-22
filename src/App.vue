2de3b7e432ced5f973a73250e4ab59660fa5ddfc
zarab <musfiqzarab@iut-dhaka.edu>
2019-08-22 18:36:30 +0600

    emit custom event(communicate from parent to child)

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,28 @@
+
 <template>
-  <h3>Hi There!</h3>
+  <div>
+    <!-- listen for events coming from child (SearchBar) -->
+    <!-- if termChange is triggered run onTermChange -->
+    <SearchBar @termChange="onTermChange"></SearchBar>
+  </div>
 </template>
 
 <script>
+import SearchBar from "./components/SearchBar";
+
 export default {
   // this object contains properties which customize this vue component &  how it behaves
-  name: "App"
+  name: "App",
+  methods: {
+    onTermChange(searchTerm) {
+      //value of 1st arg of this is 2nd org of the emited event function from child
+      console.log(searchTerm);
+    }
+  },
+
+  // here we are specifyig the components we are using here
+  components: {
+    SearchBar
+  }
 };
 </script>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- for input event we run onInput function -->
+    <!-- @ === v-on: -->
+    <input type="text" @input="onInput" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  // method is for changing the data inside of the application
+  methods: {
+    // we have functions inside of this
+    onInput(event) {
+      // emiting event to communicate with parent
+      this.$emit("termChange", event.target.value); //1st arg: name of the event to emit ,2nd: info about the event
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
