f613536ba8ebc274ee41d2cf0691ce15fe88eef6
zarab <musfiqzarab@iut-dhaka.edu>
2019-08-22 17:10:27 +0600

    writting from scratch

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+// this is the first file needed to the initial boot (entry point)
 
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App";
 
+// create vue instance
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  //   el: "#app", // similar to $mount("#app")
+  // here h is abbriviation of createElement function(we can write createElement instead of h)
+  // App component is turned into vue instance, which turns into HTML (use this HTML on the application screen)
+  render: h => h(App) // tell the instance what we wanna see on the screen(DOM)
+}).$mount("#app");
