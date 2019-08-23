3f3b79106f3e69a500dc6cf579726fbfb7d5147e
zarab <musfiqzarab@iut-dhaka.edu>
2019-08-23 14:42:51 +0600

    listing individual items

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,13 +1,19 @@
 <template>
   <ul>
-    VideoList
-    {{videos.length}}
+    <!-- need to provide the key property in the element of which we are making the list of (iteration) -->
+    <!-- key is used whenever out list is updated (update our list in a performant fasion) -->
+    <VideoListItem v-for="singleVideo in videos" :video="singleVideo" v-bind:key="video.etag" />
   </ul>
 </template>
 
 <script>
+import VideoListItem from "./VideoListItem";
+
 export default {
   name: "VideoList",
+  components: {
+    VideoListItem
+  },
   //tell component what data it should except from its parent (array of property names expecting to receive from the parent )
   // we can use obeject to validate the prop type also
   //    we can write--- props: ["videos"] or
---
+++ src/components/VideoListItem.vue
@@ -0,0 +1,11 @@
+<template>
+  <li>{{video.snippet.title}}</li>
+</template>
+
+<script>
+export default {
+  name: "VideoListItem",
+  // we are using array of strings for props
+  props: ["video"]
+};
+</script>
