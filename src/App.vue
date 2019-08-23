5060681edd8a93c07b9730100987897429e2f97b
zarab <musfiqzarab@iut-dhaka.edu>
2019-08-23 12:48:04 +0600

    pass data as props

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,38 @@
   <div>
     <!-- listen for events coming from child (SearchBar) -->
     <!-- if termChange is triggered run onTermChange -->
-    <SearchBar @termChange="onTermChange"></SearchBar>
+    <SearchBar @termChange="onTermChange" />
+    <!-- add v-bind directive to send data from parent to child -->
+    <!-- v-bind means if videos property is updated inside of perent, re-render, pass the new list to the child -->
+    <!-- v-bind means :  -->
+    <VideoList :videos="videos" />
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import SearchBar from "./components/SearchBar";
+import VideoList from "./components/VideoList";
 
 const API_KEY = "AIzaSyDk1jmFPY0NSOmkXPvTcBnDkNyDqjx1lTQ";
 
 export default {
   // this object contains properties which customize this vue component &  how it behaves
   name: "App",
+  // here we are specifyig the components we are using here
+  components: {
+    SearchBar,
+    VideoList
+  },
+  // in vue component data property is a function which returns object ( every instance get a copy of the data, not sharing a single one)
+
+  data() {
+    // function returns data object
+    return {
+      videos: []
+    };
+  },
+
   methods: {
     async onTermChange(searchTerm) {
       //value of 1st arg of this is 2nd org of the emited event function from child
@@ -30,14 +49,9 @@ export default {
           }
         }
       );
-      console.log(response);
-      // .then(response => console.log(response));
+      // saving response to video data
+      this.videos = response.data.items;
     }
-  },
-
-  // here we are specifyig the components we are using here
-  components: {
-    SearchBar
   }
 };
 </script>
---
+++ src/components/VideoList.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul>
+    VideoList
+    {{videos.length}}
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "VideoList",
+  //tell component what data it should except from its parent (array of property names expecting to receive from the parent )
+  // we can use obeject to validate the prop type also
+  //    we can write--- props: ["videos"] or
+  props: {
+    videos: Array
+  }
+};
+</script>
+
+<style scoped>
+</style>
